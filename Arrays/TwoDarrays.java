436af9fd042929d942823eb164d05b73424c924e
Aishani Goyal <aishanigoyal2005@gmail.com>
2025-06-09 23:21:53 +0530

    Search Operation in 2D Array

--- Arrays/TwoDarrays.java
+++ Arrays/TwoDarrays.java
@@ -1,6 +1,20 @@
 import java.util.*;
 
 public class TwoDarrays {
+
+    // Searching an element in 2D Array
+    public static boolean search (int matrix[][], int key){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == key){
+                    System.out.print("Key found at index (" + i + "," + j + ")");
+                    return true;
+                }
+            }
+        }
+        System.out.print("Key not found");
+        return false;
+    }
     
     public static void main (String[] args){
         int matrix[][] = new int[3][3];
@@ -23,6 +37,7 @@ public class TwoDarrays {
             }
             System.out.println();
         }
+
+        search(matrix, 4);
     }
-    
 }
