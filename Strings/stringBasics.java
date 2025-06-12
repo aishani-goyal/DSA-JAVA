f7c8bf34c6771e638e6abfb6da9a47a551502fb4
Aishani Goyal <aishanigoyal2005@gmail.com>
2025-06-12 18:40:43 +0530

    Print letters of a String

--- Strings/stringBasics.java
+++ Strings/stringBasics.java
@@ -1,5 +1,11 @@
 import java.util.*;
 public class stringBasics {
+
+    public static void printLetters(String str){
+        for (int i=0; i<str.length(); i++){
+            System.out.print(str.charAt(i) + " ");
+        }
+    }
     public static void main (String args[]){
 
         char arr[] = {'a', 'b', 'c', 'd'};
@@ -9,11 +15,19 @@ public class stringBasics {
        // Strings are IMMUTABLE - Purani String mein koi change ni ho sakta, humesha new string banti hai with new change
 
        //Input in String
-        Scanner sc = new Scanner (System.in);
+        //Scanner sc = new Scanner (System.in);
         //String name = sc.next(); //Takes only a single word
-        String name = sc.nextLine();   //Takes complete line
-        System.out.println(name);  //Output
+        //String name = sc.nextLine();   //Takes complete line
+        //System.out.println(name);  //Output
 
+        //String fullName = "Aishani Goyal";
+        //System.out.println(fullName.length());  //Strings mein length function hota hai islie () aata hai
+
+        //Concatenation
+        String firstName = "Aishani";
+        String lastName = "Goyal";
+        String fullName = firstName + " " + lastName;
+        printLetters(fullName);
     }
     
 }
