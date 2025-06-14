9d4459b6fc48c2939eb8cc5fd79097d375536b45
Aishani Goyal <aishanigoyal2005@gmail.com>
2025-06-14 22:22:52 +0530

    Palindrome

--- Strings/palindrome.java
+++ Strings/palindrome.java
@@ -12,7 +12,7 @@ public class palindrome {
     }
 
     public static void main (String[] args){
-        String str = "RACECAR";
+        String str = "NOON";
         System.out.print(isPalindrome(str));
 
     }
