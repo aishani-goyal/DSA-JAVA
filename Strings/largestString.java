b294eadb89319aab2f065be264abece9acbb0473
Aishani Goyal <aishanigoyal2005@gmail.com>
2025-06-17 22:23:51 +0530

    Lexicographical Order Largest

--- Strings/largestString.java
+++ Strings/largestString.java
@@ -1,7 +1,15 @@
 public class largestString {
 
     public static void main(String[] args) {
-        String fruits[] = {"apple", "mango", "banana"};
+        String fruits[] = {"apple", "mango", "banana"};   // Lexigographical Order Largest String
+
+        String largest = fruits[0];
+        for (int i=1; i<fruits.length; i++){
+            if (largest.compareTo(fruits[i]) < 0){
+                largest = fruits[i];
+            }
+        }
+        System.out.println(largest);
     }
     
 }
