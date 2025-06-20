931efb8a4d46090dc2684b879db08690b1ecbfec
Aishani Goyal <aishanigoyal2005@gmail.com>
2025-06-20 18:43:37 +0530

    Ones Complement

--- Bit Manipulation/bitwiseOperators.java
+++ Bit Manipulation/bitwiseOperators.java
@@ -3,7 +3,8 @@ public class bitwiseOperators {
     public static void main(String[] args) {
         //System.out.println(5 & 6);
         //System.out.println(5 | 6);
-        System.out.println(5 ^ 6);
+        //System.out.println(5 ^ 6);
+        System.out.println(~0);   //Binary One's Complement
     }
     
 }
