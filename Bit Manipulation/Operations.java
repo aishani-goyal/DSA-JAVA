bb8f6a7452f9d4fcde9f5bf7436c7b0f3430f6ce
Aishani Goyal <aishanigoyal2005@gmail.com>
2025-06-22 21:54:44 +0530

    Find ith bit in binary number

---
+++ Bit Manipulation/Operations.java
@@ -0,0 +1,17 @@
+public class Operations {
+
+    public static int ithBit(int n, int i){
+        int bitMask = 1 << i;
+        if ((n & bitMask) == 0){
+            return 0;
+        } else{
+            return 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ithBit(10, 3));
+        
+    }
+    
+}
