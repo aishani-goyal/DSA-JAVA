72b80dc762e5efcc9d37fc18964c9d8ec5187279
Aishani Goyal <aishanigoyal2005@gmail.com>
2025-06-24 22:22:28 +0530

    Clear Bits in Range

--- Bit Manipulation/Operations.java
+++ Bit Manipulation/Operations.java
@@ -1,44 +1,53 @@
 public class Operations {
 
-    public static int getithBit(int n, int i){
+    public static int getithBit(int n, int i) {
         int bitMask = 1 << i;
-        if ((n & bitMask) == 0){
+        if ((n & bitMask) == 0) {
             return 0;
-        } else{
+        } else {
             return 1;
         }
     }
-    
+
     public static int setithBit(int n, int i) {
         int bitMask = 1 << i;
         return (n | bitMask);
     }
-    
+
     public static int clearithBit(int n, int i) {
         int bitMask = ~(1 << i);
         return (n & bitMask);
     }
 
     public static int updateithBit(int n, int i, int newBit) {
-        /*if (newBit == 0){
-            return clearithBit(n, i);
-        } else{
-            return setithBit(n, i);
-        }*/
+        /*
+         * if (newBit == 0){
+         * return clearithBit(n, i);
+         * } else{
+         * return setithBit(n, i);
+         * }
+         */
 
         n = clearithBit(n, i);
         int bitMask = newBit << i;
         return (n | bitMask);
     }
 
-    public static int clearIBits(int n, int i){
-        int bitMask = (~0)<<i;    // (~0 << i) or (-1 << i)
+    public static int clearIBits(int n, int i) {
+        int bitMask = (~0) << i; // (~0 << i) or (-1 << i)
         return (n & bitMask);
     }
 
-    public static void main(String[] args) {
-        System.out.println(clearIBits(15, 2));
-        
+    public static int clearBitsInRange(int n, int i, int j){
+        int a = (~0)<<(j+1);
+        int b = (1<<i)-1;
+        int bitMask = a|b;
+        return n & bitMask;
     }
-    
+
+    public static void main(String[] args) {
+        System.out.println(clearBitsInRange(10, 2, 4));
+
+    }
+
 }
