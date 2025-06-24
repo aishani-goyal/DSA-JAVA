b5cb9642cdf2c21517db3453b8113644dd3bc6de
Aishani Goyal <aishanigoyal2005@gmail.com>
2025-06-24 22:46:42 +0530

    Count set bits in a number

--- Bit Manipulation/Operations.java
+++ Bit Manipulation/Operations.java
@@ -46,11 +46,23 @@ public class Operations {
     }
 
     public static boolean isPowerOfTwo(int n){
-        return (n&(n-1)) == 0;
+        return n>0 && (n&(n-1)) == 0;
+    }
+
+    public static int countSetBits(int n){    // Time Complexity: O(log(n))
+        int count = 0;
+
+        while (n > 0){
+            if ((n & 1) != 0){   //Checking LSB
+                count++;
+            }
+            n = n>>1;  //Right shift by 1 bit
+        }
+        return count;
     }
 
     public static void main(String[] args) {
-        System.out.println(isPowerOfTwo(7));
+        System.out.println(countSetBits(10));
 
     }
 
