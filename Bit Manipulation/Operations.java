8cd7d50e5c2a32a6447295fcabbb1e98203ef8be
Aishani Goyal <aishanigoyal2005@gmail.com>
2025-06-25 23:40:28 +0530

    Bit Manipulation Question

--- Bit Manipulation/Operations.java
+++ Bit Manipulation/Operations.java
@@ -62,7 +62,7 @@ public class Operations {
     }
 
     public static void main(String[] args) {
-        System.out.println(countSetBits(14));
+        System.out.println(countSetBits(18));
 
     }
 
