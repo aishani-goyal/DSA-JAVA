ca649260bb52a4fe67e33f2146da440a91048453
Aishani Goyal <aishanigoyal2005@gmail.com>
2025-06-26 22:28:07 +0530

    Function updated

--- Bit Manipulation/Operations.java
+++ Bit Manipulation/Operations.java
@@ -62,7 +62,7 @@ public class Operations {
     }
 
     public static void main(String[] args) {
-        System.out.println(countSetBits(25));
+        System.out.println(countSetBits(27));
 
     }
 
