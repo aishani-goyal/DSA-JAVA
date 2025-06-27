be77f7ac5de02d3449136792d04dc0a43c030817
Aishani Goyal <aishanigoyal2005@gmail.com>
2025-06-27 22:15:41 +0530

    Fast Exponentiation

--- Bit Manipulation/Operations.java
+++ Bit Manipulation/Operations.java
@@ -61,8 +61,22 @@ public class Operations {
         return count;
     }
 
+    public static int fastExpo(int a, int n){
+        int ans = 1;
+
+        while (n > 0){
+
+            if ((n & 1) != 0){   //Check LSB
+                ans  = ans * a;
+            }
+            a = a * a;
+            n = n >> 1;
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
-        System.out.println(countSetBits(29));
+        System.out.println(fastExpo(3, 5));
 
     }
 
