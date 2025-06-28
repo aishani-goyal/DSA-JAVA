9ad3040184a8148351f2c883d3b8c7c4e912c9a1
Aishani Goyal <aishanigoyal2005@gmail.com>
2025-06-28 23:27:49 +0530

    Exponentiation

--- Bit Manipulation/Operations.java
+++ Bit Manipulation/Operations.java
@@ -76,7 +76,7 @@ public class Operations {
     }
 
     public static void main(String[] args) {
-        System.out.println(fastExpo(4, 5));
+        System.out.println(fastExpo(2, 5));
 
     }
 
