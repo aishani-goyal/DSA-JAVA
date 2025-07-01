038b47f19d9f04b783ab81e0b27c0cc9e33f79e3
Aishani Goyal <aishanigoyal2005@gmail.com>
2025-07-01 22:48:10 +0530

    XOR

--- Bit Manipulation/bitwiseOperators.java
+++ Bit Manipulation/bitwiseOperators.java
@@ -6,7 +6,7 @@ public class bitwiseOperators {
         //System.out.print(5 ^ 6);
         //System.out.print(~0);   //Binary One's Complement
         //System.out.print(5 << 2);   //Binary Left Shift (Formula: a<<b -> a * 2^b)  (Left Shift bits by 'b' positions and fill remaining right bits by 0)
-        System.out.print(6 >> 1); // Binary Right Shift (Formula: a<<b -> a / 2^b) (Right Shift bits by 'b'positions and fill remaining left bits by 0)
+        System.out.print(7 >> 2); // Binary Right Shift (Formula: a<<b -> a / 2^b) (Right Shift bits by 'b'positions and fill remaining left bits by 0)
     }
     
 }
