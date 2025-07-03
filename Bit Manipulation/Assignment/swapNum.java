e2eda00780e2538d9ee8ec6127b1652c3e830ca1
Aishani Goyal <aishanigoyal2005@gmail.com>
2025-07-03 23:35:48 +0530

    Swapping numbers without third variable

---
+++ Bit Manipulation/Assignment/swapNum.java
@@ -0,0 +1,17 @@
+package Assignment;
+
+public class swapNum {
+
+    public static void main(String[] args) {
+
+        // Swap two numbers without using any third variable
+        int x = 3, y = 4;
+        System.out.println("Before swap:\n" + "x = " + x + ", y = " + y);
+        x = x ^ y;
+        y = x ^ y;
+        x = x ^ y; 
+
+        System.out.println("\nAfter swap:\n" + "x = " + x + ", y = " + y);
+    }
+    
+}
