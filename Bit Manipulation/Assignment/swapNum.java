949ff00958e90c44e7a04be7d81a5b9c53fa2148
Aishani Goyal <aishanigoyal2005@gmail.com>
2025-07-04 23:39:53 +0530

    updated code

--- Bit Manipulation/Assignment/swapNum.java
+++ Bit Manipulation/Assignment/swapNum.java
@@ -5,7 +5,7 @@ public class swapNum {
     public static void main(String[] args) {
 
         // Swap two numbers without using any third variable
-        int x = 100, y = 50;
+        int x = 10, y = 50;
         System.out.println("Before swap:\n" + "x = " + x + ", y = " + y);
         x = x ^ y;
         y = x ^ y;
