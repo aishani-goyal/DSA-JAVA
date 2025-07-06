ae1433679aaba8dfd9bf731f2c94664ac1837979
Aishani Goyal <aishanigoyal2005@gmail.com>
2025-07-06 22:32:20 +0530

    OOPS class created

--- OOPS/FirstClass.java
+++ OOPS/FirstClass.java
@@ -1,5 +1,11 @@
 public class FirstClass {
 
+    public static void main (String[] args){
+        Pen p1 = new Pen();  //created a pen object called p1
+        p1.setColor("Blue");
+        System.out.println(p1.color);
+    }
+
 }
 
 class Pen{
