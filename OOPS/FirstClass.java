71f7b5a27fde9e5d5d9eeb3f899a05fc9a997140
Aishani Goyal <aishanigoyal2005@gmail.com>
2025-07-06 22:13:21 +0530

    Classes in OOP

--- OOPS/FirstClass.java
+++ OOPS/FirstClass.java
@@ -3,5 +3,16 @@ public class FirstClass {
 }
 
 class Pen{
-    //prop + func
+    //Blueprint of pen
+
+    String color;
+    int tip;
+
+    void setColor(String newColor){
+        color = newColor;
+    }
+
+    void setTip(int newTip){
+        tip = newTip;
+    }
 }
