8e4e6f0ca40f8581f6326706b331dfe370b6bdbc
Aishani Goyal <aishanigoyal2005@gmail.com>
2025-07-21 23:43:23 +0530

    Bubble sort practice

--- Sorting Algorithms/bubbleSort.java
+++ Sorting Algorithms/bubbleSort.java
@@ -22,7 +22,7 @@ public class bubbleSort{
     }
 
     public static void main (String[] args){
-        int arr[] = {5, 4, 1, 3, 2};
+        int arr[] = {5, 7, 1, 3, 2};
 
         sort(arr);
         printArr(arr);
