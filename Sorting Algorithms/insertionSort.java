a7f819de7531e827de651f4806de720ef5bfea02
Aishani Goyal <aishanigoyal2005@gmail.com>
2025-07-21 23:44:37 +0530

    Insertion sort practice

--- Sorting Algorithms/insertionSort.java
+++ Sorting Algorithms/insertionSort.java
@@ -26,7 +26,7 @@ public class insertionSort {
     }
 
     public static void main(String[] args) {
-        int arr[] = { 5, 4, 1, 3, 2 };
+        int arr[] = { 5, 6, 1, 3, 2 };
 
         insertion(arr);
         printArr(arr);
